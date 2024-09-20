0cd0a2186bd8e65a3751900caa22c2fe07bac8cd
Дмитрий Чехонадских <112265709+loseex@users.noreply.github.com>
2024-09-20 22:18:11 +0500

    Добавил дочерние компоненты в компонент Navbar
    - Добавил компонент NavbarItem
    - Добавил компонент NavbarSection
    - Поправил типы для props объектов
    - Убрал лишние импорты
    - Изменил стиль экспорта компонента

--- src/widgets/ui/navbar.tsx
+++ src/widgets/ui/navbar.tsx
@@ -1,10 +1,10 @@
-import React, { PropsWithChildren } from "react";
+import React from "react";
 
 export interface NavbarProps extends React.HTMLAttributes<HTMLDivElement> {}
+export interface NavbarSectionProps
+  extends React.HTMLAttributes<HTMLUListElement> {}
 export interface NavbarItemProps
-  extends React.FC<
-    PropsWithChildren<React.HTMLAttributes<HTMLButtonElement>>
-  > {}
+  extends React.HtmlHTMLAttributes<HTMLLIElement> {}
 
 /**
  * Navbar
@@ -14,35 +14,68 @@ export interface NavbarItemProps
  * Он обеспечивает гибкую и адаптивную структуру,
  * позволяя пользователям легко перемещаться по различным разделам вашего приложения или сайта.
  *
- * @param Item Элемент навигации на веб-странице
+ * @param props - Дополнительные атрибуты для настройки стилей и поведения компонента.
  *
  * @author loseex
  */
-
-const Navbar: React.FC<NavbarProps> & { Item: NavbarItemProps } = (props) => {
+const NavbarComponent: React.FC<NavbarProps> = (props): React.ReactElement => {
   return (
-    <div className="w-full h-[60px] flex justify-center items-center p-3">
-      <div className="w-full max-w-[1280px] px-4 flex justify-between items-center">
-        <div></div>
-        <div className="w-fit flex flex-row gap-3 justify-center items-center">
-          {props.children}
-        </div>
-      </div>
+    <div
+      className="w-full h-[60px] flex justify-center items-center p-3"
+      {...props}
+    >
+      <div
+        className="w-full max-w-[1280px] px-4 flex justify-between items-center"
+        children={props.children}
+      />
     </div>
   );
 };
-Navbar.displayName = "Navbar";
+NavbarComponent.displayName = "Navbar";
 
 /**
- * Navbar Item
+ * NavbarItem
  *
- * Это элемент навигации на веб-странице.
+ * Компонент NavbarItem оборачивает дочерние элементы секции в тег <li>.
+ * Это позволяет легко добавлять элементы навигации в секцию,
+ * сохраняя при этом единообразный стиль и структуру.
  *
- * todo
- *
- * @param children
+ * @param props - Дополнительные атрибуты для настройки стилей и поведения компонента.
  */
+const NavbarItem: React.FC<NavbarItemProps> = (props) => (
+  <li children={props.children} />
+);
+NavbarItem.displayName = "Navbar.Item";
 
-Navbar.Item = ({ children }): React.ReactElement => <a href="#">{children}</a>;
+/**
+ * NavbarSection
+ *
+ * Компонент NavbarSection представляет собой секцию компонентов на Navbar.
+ * Он используется для группировки элементов навигации в отдельные секции,
+ * обеспечивая более организованное представление навигационных элементов.
+ *
+ * @param props - Дополнительные атрибуты для настройки стилей и поведения компонента.
+ */
+const NavbarSection: React.FC<NavbarSectionProps> = (
+  props
+): React.ReactElement => {
+  /**
+   * Момент обертки всех children в тег.
+   */
+  const updated = React.Children.map(props.children, (child) => (
+    <Navbar.Item children={child} />
+  ));
+  return <ul className="flex flex-row gap-3" children={updated} />;
+};
+NavbarSection.displayName = "Navbar.Section";
 
-export { Navbar };
+/**
+ * Navbar
+ *
+ * Основной компонент, объединяющий NavbarComponent, NavbarSection и NavbarItem,
+ * позволяя легко создавать адаптивные навигационные панели с использованием вложенной структуры.
+ */
+export const Navbar = Object.assign(NavbarComponent, {
+  Section: NavbarSection,
+  Item: NavbarItem,
+});
