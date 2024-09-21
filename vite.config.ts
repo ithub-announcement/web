f120db6f4fb634dced439b2f4e8fbb5a2ea76c21
Дмитрий Чехонадских <112265709+loseex@users.noreply.github.com>
2024-09-21 15:53:16 +0500

    Изменить конфигурацию сборки проекта
    - Добавил предупреждение при нарушении ограничения размера чанка
    - Добавил параметр для выбора варианта минификации файлов

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 600,
+    minify: "esbuild",
+  },
   server: {
     watch: {
       usePolling: true,
