9bb5f2c84a286a52320157bb8c875633df3b7ffc
Дмитрий Чехонадских <112265709+loseex@users.noreply.github.com>
2024-09-27 00:24:26 +0500

    Добавлен редюсер для изменения состояния редактора
    - Добавил начальный стейт для wrapper
    - Добавлен редюсер setState, позволяющий изменять значение определенного блока по его id
    - Добавил комментарии к редюсерам

--- src/app/store/slices/editor/editor.slice.ts
+++ src/app/store/slices/editor/editor.slice.ts
@@ -1,9 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState = {
   wrapper: {
     title: "",
-    content: [],
+    content: [
+      {
+        id: 0,
+        type: "paragraph",
+        value: "",
+      },
+    ],
   },
   origin: {
     uuid: "",
@@ -19,6 +25,26 @@ export const EditorSlice = createSlice({
   name: "editor/slice",
   initialState,
   reducers: {
+    /**
+     * Метод для изменения данных у определенного блока.
+     *
+     * @param id
+     * @param value
+     */
+    setState: (
+      _state,
+      action: PayloadAction<{ id: number; value: string }>
+    ) => {
+      const c = _state.wrapper.content.find((x) => x.id === action.payload.id);
+      if (c) {
+        c.value = action.payload.value;
+      }
+    },
+
+    /**
+     * Метод для сброса данных у редактора.
+     * @default
+     */
     reset: (_state) => {
       _state = initialState;
     },
